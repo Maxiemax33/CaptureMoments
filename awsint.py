38f6ffc5bb6510006989ab9109c12b638dec3703
Maxiemax33 <shaniyajoseph29@gmail.com>
2025-06-30 11:16:58 +0530

    Update awsint.py

--- awsint.py
+++ awsint.py
@@ -58,4 +58,4 @@ def show_photographers():
                            availability_data=availability_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
